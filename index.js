089f366c6d612e78974a7db573a97167cd865dac
Adnan <adnan.chowdhury@outlook.com>
2019-09-10 16:00:52 -0500

    POST endpoint created for DB

--- index.js
+++ index.js
@@ -15,7 +15,22 @@ server.get("/", (req, res) => {
     });
 });
 
-server.post("/", (req, res) => {});
+server.post("/", (req, res) => {
+  const carData = req.body;
+  db("cars")
+    .insert(carData)
+    .then(ids => {
+      db("cars")
+        .where({ id: ids[0] })
+        .then(newFruitEntry => {
+          res.status(201).json(newFruitEntry);
+        });
+    })
+    .catch(err => {
+      console.log("POST error", err);
+      res.status(500).json({ message: "Failed to store data" });
+    });
+});
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
